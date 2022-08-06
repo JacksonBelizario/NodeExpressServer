6a79237e0d3ea26d9e1a15b53ab5e9a9ee9f95f1
Jackson Belizário <jacksonluizzb@gmail.com>
2022-08-06 08:45:25 -0400

    Create CRUD for user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,51 @@
+const users = []
+let id = 0
 
-function index(req, res) {}
+function index(req, res) {
+    res.json(users)
+}
 
-function store(req, res) {}
+function store({ body }, res) {
+    const user = {
+        id: ++id,
+        name: body.name,
+        email: body.email
+    }
 
-function update(req, res) {}
+    users.push(user)
 
-function destroy(req, res) {}
+    return res.json(user)
+}
+
+function update({ params, body }, res) {
+    const index = users.findIndex(u => u.id == params.id)
+
+    if (index === -1) {
+        return res.status(404).json({ response: 'ERROR' })
+    }
+
+    const user = {
+        ...users[index],
+        name: body.name,
+        email: body.email
+    }
+
+    users[index] = user
+
+    return res.json(user)
+}
+
+function destroy({ params }, res) {
+    const index = users.findIndex(u => u.id == params.id)
+
+    if (index === -1) {
+        return res.status(404).json({ response: 'ERROR' })
+    }
+
+    users.splice(index, 1)
+
+    return res.json({ response: 'SUCCESS' })
+}
 
 module.exports = {
     index,
